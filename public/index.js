454ab45d07b717b04bb122c47e96a96227dc9995
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-01-29 20:28:17 +0200

    first temp start

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
 console.log('Hello world from client-side JS!')
+import { ApolloServer } from 'apollo-server';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
